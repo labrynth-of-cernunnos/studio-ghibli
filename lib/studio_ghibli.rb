e25189da8825c94e33a0a5f97c696540cda33185
labrynth-of-cernunnos <labrynth-of-cernunnos@protonmail.com>
2020-07-15 11:51:39 -0700

    setting up rspec

--- lib/studio_ghibli.rb
+++ lib/studio_ghibli.rb
@@ -2,13 +2,12 @@ require 'open-uri'
 require 'nokogiri'
 require 'httparty'
 
-require_relative "./studio_ghibli/findable"
-
-require_relative "./studio_ghibli/version"
-require_relative "./studio_ghibli/cli"
-require_relative "./studio_ghibli/api"
-require_relative "./studio_ghibli/film"
-require_relative "./studio_ghibli/person"
-require_relative "./studio_ghibli/location"
-require_relative "./studio_ghibli/species"
-require_relative "./studio_ghibli/vehicle"
+# require_relative "./studio_ghibli/findable"
+# require_relative "./studio_ghibli/version"
+# require_relative "./studio_ghibli/cli"
+# require_relative "./studio_ghibli/api"
+# require_relative "./studio_ghibli/film"
+# require_relative "./studio_ghibli/person"
+# require_relative "./studio_ghibli/location"
+# require_relative "./studio_ghibli/species"
+# require_relative "./studio_ghibli/vehicle"
---
+++ spec/studio_ghilbi_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe StudioGhibli do
+  it "has a version number" do
+    expect(StudioGhibli::VERSION).not_to be nil
+  end
+end
